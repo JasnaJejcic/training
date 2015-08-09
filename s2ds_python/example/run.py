555bcd074cf3cfc96259fcff154b46b6d48d4f2e
Prash <prash@pelucid.com>
2015-08-09 22:55:35 +0100

    updates

---
+++ s2ds_python/example/run.py
@@ -0,0 +1,21 @@
+"""
+Test Module for training.
+
+Usage:
+    run.py create
+    run.py fetch <url>
+
+Options
+    url     URL to fetch
+"""
+import docopt
+
+import app.main as app
+
+if __name__ ==  "__main__":
+    args = docopt.docopt(__doc__)
+
+    if args['create']:
+        app.create()
+    elif args['fetch']:
+        app.fetch(args['--url'])
